918152f8e1936380a61fba44f14a20e73253fc01
Ashish Goswami <avengersfan60@gmail.com>
2024-12-12 22:09:33 +0530

    changes

---
+++ src/Components/AnimatedTexts/AnimatedLetters.js
@@ -0,0 +1,15 @@
+import "./AnimatedTexts.scss"
+
+export default function AnimatedLetters({letterClass, strArray, idx}){
+    return(
+        <span>
+            {
+                strArray.map((char,i)=>(
+                    <span key={char + i} className={`${letterClass} _${i + idx}`}>
+                        {char}
+                    </span>
+                ))
+            }
+        </span>
+    )
+};
